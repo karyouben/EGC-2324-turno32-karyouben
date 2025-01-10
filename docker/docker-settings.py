2a37bf894737b1b92d26252c66beb6213f7f3a98
karim youssafi benichikh <karyouben@alum.us.es>
2025-01-10 13:57:07 +0100

    añadido cambios

--- docker/docker-settings.py
+++ docker/docker-settings.py
@@ -5,7 +5,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'postgres',
         'USER': 'postgres',
-        'PASSWORD':'',
+        'PASSWORD': 'decide',
         'HOST': 'db',
         'PORT': 5432,
     }
